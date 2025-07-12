72479aafc99543360a15b07b7d52b764bc18dff7
johnrcd <80877409+johnrcd@users.noreply.github.com>
2025-07-12 12:16:01 -0500

    feat: add particles

--- public/js/themes.js
+++ public/js/themes.js
@@ -51,6 +51,10 @@ const themes = {
 			"filter": "",      // css filter
 			"border" : "",     // css border
 		},
+
+		// if set, allows for particles
+		// see https://github.com/marcbruederlin/particles.js for details
+		"particles" : "" // (optional) particle.js json config
 	},
 	"midnight" : {
 		"fontNormal" : '"Manrope", "Poppins", "Open Sans", sans-serif',
@@ -80,11 +84,26 @@ const themes = {
 		},
 
 		"glass" : {
-			"background" : "rgba(70, 20, 35, 0.18)",
+			"background" : "rgba(255, 255, 255, 0.02)",
 			"radius" : "16px",
 			"shadow" : "0 4px 16px rgba(0, 0, 0, 0)",
 			"filter": "blur(3.3px)",
-			"border" : "1px solid rgba(255, 255, 255, 0.15)",
+			"border" : "1px solid rgba(255, 255, 255, 0)",
+		},
+
+		"particles" : {
+			selector: '.background',
+			color: "#1d3243",
+			connectParticles: true,
+			speed: 0.1,
+			maxParticles: 100,
+			minDistance: 120,
+			responsive: [
+				{ breakpoint: 1440, options: { maxParticles: 100, }, },
+				{ breakpoint: 1200, options: { maxParticles: 75,  }, },
+				{ breakpoint: 768,  options: { maxParticles: 50,  }, },
+				{ breakpoint: 576,  options: { maxParticles: 0,   }, },
+			],
 		},
 	},
 	"sunset" : {
@@ -120,6 +139,21 @@ const themes = {
 			"shadow" : "0 4px 16px rgba(0, 0, 0, 0)",
 			"filter": "blur(3.3px)",
 			"border" : "1px solid rgba(255, 255, 255, 0.15)",
+		},
+
+		"particles" : {
+			selector: '.background',
+			color: "AAAAAA",
+			connectParticles: true,
+			speed: 0.05,
+			maxParticles: 200,
+			minDistance: 150,
+			responsive: [
+				{ breakpoint: 1440, options: { maxParticles: 200, }, },
+				{ breakpoint: 1200, options: { maxParticles: 120, }, },
+				{ breakpoint: 768,  options: { maxParticles: 60,  }, },
+				{ breakpoint: 576,  options: { maxParticles: 25,  }, },
+			],
 		},
 	}
 }
@@ -219,6 +253,20 @@ const setTheme = (theme) => {
 			root.style.setProperty("--header-title-weight", fontWeight);
 		}
 	}
+
+	const particleOptions = themeData.hasOwnProperty("particles") ?
+		themeData.particles : {
+			selector: '.background',
+			maxParticles: 0,
+			responsive: [
+				{ breakpoint: 1440, options: { maxParticles: 0, }, },
+				{ breakpoint: 1200, options: { maxParticles: 0, }, },
+				{ breakpoint: 768,  options: { maxParticles: 0, }, },
+				{ breakpoint: 576,  options: { maxParticles: 0, }, },
+			],
+		};
+
+	Particles.init(particleOptions);
 
 	localStorage.setItem("theme_on_load", theme);
 };
